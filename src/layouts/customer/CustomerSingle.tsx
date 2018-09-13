20d4181546774db334e059a85f521e2b04afe42d
antti pitkänen <pitkan@gmail.com>
2018-09-13 16:56:18 +0300

    show single customer data

--- src/layouts/customer/CustomerSingle.tsx
+++ src/layouts/customer/CustomerSingle.tsx
@@ -1,14 +1,40 @@
 import { ICustomerSingleProps } from '../../interfaces'
 import * as React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
-export class CustomerSingle extends React.Component<ICustomerSingleProps, {}> {
-  
+const getCustomer = gql`
+  query Customer($_id: String!) {
+    customer(_id: $_id) {
+      name
+    }
+  }
+`
 
-
+class CustomerSingle extends React.Component<ICustomerSingleProps, {}> {
   public render() {
+    if (this.props.selectedCustomerId === '') {
+      return 'valitse homo'
+    }
 
     return (
-      <span>{this.props.selectedCustomerId}</span>
+      <Query query={getCustomer} variables={{_id: this.props.selectedCustomerId}}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return 'Loading...'
+          }
+  
+          if (error) {
+            return `Error! ${error.message}`
+          }
+
+          return (
+            <div>
+              {data.customer.name}
+            </div>
+          )
+        }}
+      </Query>
     )
   }
 }
