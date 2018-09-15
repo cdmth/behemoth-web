59c2d5ede155c877d330321adb81003f6c5e2898
antti pitkänen <pitkan@gmail.com>
2018-09-15 12:30:25 +0300

    customerid to createProject

--- src/modules/project/CreateProject.tsx
+++ src/modules/project/CreateProject.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react'
 import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
+import { Mutation, Query } from "react-apollo";
 import { ICreateProjectProps } from '../../interfaces'
 
-const addProject = gql`
-  mutation CreateProject($name: String) {
-    createProject(name: $name) {
+const getCustomers = gql`
+  {
+    customers {
       _id
       name
     }
   }
-`;
+`
+
+const addProject = gql`
+  mutation CreateProject($name: String, $customerId: String!) {
+    createProject(name: $name, customerId: $customerId) {
+      _id
+      name
+      customerId
+    }
+  }
+`
 
 const createProject : React.SFC<ICreateProjectProps> = (props) => {
-  let input: any
+  let projectName: any
+  let customerId: any
 
   return (
     <Mutation mutation={addProject} >
@@ -29,16 +40,38 @@ const createProject : React.SFC<ICreateProjectProps> = (props) => {
           <form
             onSubmit={e => {
               e.preventDefault();
-              create({ variables: { name: input.value } });
-              input.value = "";
+              create({ variables: { name: projectName.value, customerId: customerId.value}});
+              projectName.value = "";
             }}
-          > 
+          >  
+            <Query query={getCustomers}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return 'Loading...'
+              }
+
+              if (error) {
+                return `Error! ${error}`
+              }
+
+              return (
+                <div className="field">
+                  <label className="label">Select customer</label>
+                  <div className="select">
+                    <select ref={node => {customerId = node}}>
+                      {data.customers.map((customer:any) => (
+                        <option key={customer._id} value={customer._id}>{customer.name}</option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              )
+            }}
+            </Query>
+              
             <div className="field">
-              <input className="input"
-                ref={node => {
-                  input = node;
-                }}
-              />
+              <label className="label">Project name</label>
+              <input ref={node => {projectName = node}} className="input" placeholder="Ex. Homon Kulli -projekti" />
             </div>
             <button className="button is-primary" type="submit">Add Project</button>
           </form>
